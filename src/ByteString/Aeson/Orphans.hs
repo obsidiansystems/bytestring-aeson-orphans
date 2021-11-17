2b6fd35d8309156bd7547e4cccdbf4b3601d3510
Anurag Ohri <anuragohri92@gmail.com>
2021-11-17 17:34:54 +0530

    Removed unused imports. Removed ghc 8.0.2 since DerivingStrategies requires ghc 8.2.1

--- src/ByteString/Aeson/Orphans.hs
+++ src/ByteString/Aeson/Orphans.hs
@@ -9,15 +9,10 @@
 module ByteString.Aeson.Orphans where
 
 import Data.Aeson
-import Data.Aeson.Types (Parser)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Base64 as B64
 import qualified Data.ByteString.Lazy as LBS
-import Data.Map (Map)
-import qualified Data.Map as Map
-import Data.Monoid hiding (First (..))
 import Data.Ord
-import Data.Semigroup
 import Data.Text.Encoding (decodeUtf8, encodeUtf8)
 
 deriving newtype instance ToJSON a => ToJSON (Down a)
